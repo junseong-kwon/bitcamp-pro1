c4b469ce4638f4b3b0239cf3cd0e035d14e6aa36
junseong-kwon <rnjswnstjd13@gmail.com>
2024-07-02 17:54:18 +0900

    삭제추가

--- app/src/main/java/bitcamp/project2/Task.java
+++ app/src/main/java/bitcamp/project2/Task.java
@@ -4,19 +4,25 @@ import java.time.LocalDate;
 
 public class Task {
 
+  private static int seqNo;
+  private int no;
   private String list;
   private LocalDate date;
   private boolean completed;
 
-
   public Task(String title, LocalDate date, boolean completed) {
+    this.no = ++seqNo; // 고유 번호 자동 생성
     this.list = title;
     this.date = date;
     this.completed = completed;
   }
 
+  public int getNo() {
+    return no;
+  }
+
   @Override
   public String toString() {
-      return (completed ? "\u001B[31m ⬤ \u001B[0m" : " 〇") + "     " + list + "     " + date;
-    }
+    return String.format("%-4s %-15s %-10s", (completed ? "\u001B[31m⬤\u001B[0m" : "〇"), list, date);
   }
+}
